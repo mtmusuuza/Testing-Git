5ca1d8216be90628ba8308d5a0d890d21c1a8728
mtmusuuza <75982833+mtmusuuza@users.noreply.github.com>
2021-03-04 22:58:35 +0000

    testCenter
    testing out

---
+++ TestCenter/src/testPack/Test.java
@@ -0,0 +1,16 @@
+package testPack;
+
+public class Test {
+
+	//public Test() {
+	public static void myMealList() {
+			System.out.println("This is my meals list: ");
+		}
+	//}
+
+	public static void main(String[] args) {
+		Test tst = new Test();
+		tst.myMealList();
+	}
+
+}
